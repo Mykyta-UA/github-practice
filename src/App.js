37c197309af64d06fdb6274c25568bbe0b03f25a
mykyta stoliarov <nick.stoliarov@gmail.com>
2022-09-21 23:31:47 -0400

    first commit with changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import { useState } from "react"; // providing the feature that could track state in the app
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCardHandler() {
+    setCartIsShown = true;
+  }
+  function hideCartHandler() {
+    setCartIsShown = false;
+  }
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCardHandler} />
       <main>
         <Meals />
       </main>
